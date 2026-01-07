c9a2ced8b0be7a5ca85821d9dc3e319fdea553e6
Gabriel Oliveira Porfiro <gabrielporfiro21@gmail.com>
2026-01-07 11:34:05 -0300

    Server port adjustment

--- api/src/env.ts
+++ api/src/env.ts
@@ -2,7 +2,7 @@ import { z } from "zod"
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
-  PORT: z.coerce.number().default(333),
+  PORT: z.coerce.number().default(3333),
   
   DATABASE_URL: z.url(),  
 })
